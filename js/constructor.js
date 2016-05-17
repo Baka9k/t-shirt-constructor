b265d0efe048a9e9ca05ea46b11fa54ac3fff71d
Baka9k <neoplanspaceliner9@gmail.com>
2016-05-17 17:41:15 +0300

    Templates rendering works

--- js/constructor.js
+++ js/constructor.js
@@ -1,46 +1,56 @@
 var editor = {};
 
 
-editor.preparePage = function() {
-	var width = $("#canvas1").width();
-	var height = width / 0.96;
-	$("#canvas1").height(height);
-	$("#canvas2").height(height);
-	$("#canvas3").height(height);
-}
 
-editor.createCanvas = function(element) {
-	var canvas = $('<canvas/>').appendTo(element);
-	canvas.width($(element).width());
-	canvas.height($(element).height());
-	return canvas;
-}
-
+editor.variables = {
+	canvasWidth: 0,
+	canvasHeight: 0,
+};
 editor.canvases = {};
 editor.contexts = {};
 //editor.canvases: front, rear, sides
 //editor.contexts: front, rear, sides
 
+
+
+editor.preparePage = function() {
+	var width = $("#canvasDiv1").width();
+	var height = width / 0.96;
+	$("#canvasDiv1").height(height);
+	$("#canvasDiv2").height(height);
+	$("#canvasDiv3").height(height);
+	editor.variables.canvasWidth = width;
+	editor.variables.canvasHeight = height;
+}
+
+editor.createCanvas = function(element) {
+	var canvas = document.createElement('canvas');
+	element.appendChild(canvas);
+	canvas.setAttribute('width', $(element).width());
+	canvas.setAttribute('height', element.style.height);
+	return canvas;
+}
+
+
+
 editor.drawMasks = function() {
 	for (var ctx in editor.contexts) {
 		var texture = new Image();
 		texture.src = "images/" + ctx + ".png";
-		texture.onload = function() {
-			return function () {
-				var texture = texture;
-				editor.contexts[ctx].drawImage(texture, 0, 0, 100, 100);
-			}
-		}
+		texture.onload = (function (texture, ctx) { return function () {
+			editor.contexts[ctx].drawImage(texture,  0, 0, editor.variables.canvasWidth, editor.variables.canvasHeight);
+		} })(texture,ctx);
+		console.log(editor.variables.canvasWidth, editor.variables.canvasHeight);
 	}
 }
 
 editor.init = function () {
 	editor.preparePage();
-	editor.canvases.front = editor.createCanvas($("#canvas1"));
-	editor.canvases.rear = editor.createCanvas($("#canvas2"));
-	editor.canvases.sides = editor.createCanvas($("#canvas3"));
+	editor.canvases.front = editor.createCanvas(document.getElementById("canvasDiv1"));
+	editor.canvases.rear = editor.createCanvas(document.getElementById("canvasDiv2"));
+	editor.canvases.sides = editor.createCanvas(document.getElementById("canvasDiv3"));
 	for (var canvas in editor.canvases) {
-		editor.contexts[canvas] = editor.canvases[canvas][0].getContext("2d");
+		editor.contexts[canvas] = editor.canvases[canvas].getContext("2d");
 	}
 	editor.drawMasks();
 }
