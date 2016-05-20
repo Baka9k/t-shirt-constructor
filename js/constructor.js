ae3c43d675690cb7fb909ce46fb0ab356a124fea
Baka Cirno <neoplanspaceliner9@gmail.com>
2016-05-21 00:37:40 +0300

    Modals

--- js/constructor.js
+++ js/constructor.js
@@ -63,7 +63,7 @@ editor.useTool = function(tool) {
 }
 
 editor.openModal = function() {
-	$("#addtextmodal").modal();
+	$("#modal").modal();
 }
 
 
@@ -89,7 +89,7 @@ editor.tools = {
 	addtext: function() {
 		ReactDOM.render(
 			<addtext />,
-			document.getElementById('modalContainer')
+			document.getElementById('modalBody')
 		);
 	},
 	addpicture: function() {
@@ -134,21 +134,8 @@ $(document).ready(function() {
 var addtext = React.createClass({
 	render: function() {
 		return (
-			<div class="modal fade" id="addtextmodal" role="dialog">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal">&times;</button>
-							<h4 class="modal-title">Modal Header</h4>
-						</div>
-						<div class="modal-body">
-							<p>Some text in the modal.</p>
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						</div>
-					</div>
-			  	</div>
+			<div>
+			irsduozdsihgoi
 			</div>
 		);
 	}
